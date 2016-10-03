f42288001a48e84dcba3c55ceadab7228825cc87
bangkuder <dhery@roketdigital.com>
2016-10-03 14:56:49 +0700

    test lagi

--- filter-content.php
+++ filter-content.php
@@ -1,10 +1,10 @@
 <?php
-/* 
-Template Name: filter Content Template
+/*
+Template Name: filter Content Template oke
 */
 ?>
 <?php
-$category = get_the_category(); 
+$category = get_the_category();
 ?>
 <div class="btn-group bootstrap-select sorting">
 	<button type="button" class="btn dropdown-toggle selectpicker button-sort" data-toggle="dropdown" data-id="sort_download" title="Mustard" aria-expanded="true">
@@ -12,38 +12,38 @@ $category = get_the_category();
 		      $paramSort = $category[0]->cat_name;
 		      switch ($paramSort) {
 		        case 'Artikel dari Ibu':
-		          ?> 
+		          ?>
 		          	<span class="filter-option pull-left">Artikel dari Ibu</span>&nbsp;
 		          <?php
 		          break;
 		        case 'Kebersihan Si Kecil':
-		          ?> 
+		          ?>
 		          	<span class="filter-option pull-left">Kebersihan Si Kecil</span>&nbsp;
 		          <?php
 		          break;
 		        case 'Kebersihan Ibu':
-		          ?> 
+		          ?>
 		          	<span class="filter-option pull-left">Kebersihan Ibu</span>&nbsp;
 		          <?php
 		          break;
 		        case 'Kebersihan Rumah':
-		          ?> 
+		          ?>
 		          	<span class="filter-option pull-left">Kebersihan Rumah</span>&nbsp;
 		          <?php
 		          break;
 		        case 'Bepergian':
-		          ?> 
+		          ?>
 		          	<span class="filter-option pull-left">Bepergian</span>&nbsp;
 		          <?php
 		          break;
-		         
+
 		        default:
-		          ?> 
+		          ?>
 		          	<span class="filter-option pull-left">Artikel dari Ibu</span>&nbsp;
 		          <?php
 		          break;
 		      }
-		      ?> 
+		      ?>
 		<span class="caret"></span>
 	</button>
 	<?php wp_nav_menu(array(
