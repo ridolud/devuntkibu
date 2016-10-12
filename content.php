44044ed4dbf62f3ffc7e87d88d001ff0beb6d555
ridolud <faridho@gmx.com>
2016-10-12 17:38:09 +0700

    ok

--- content.php
+++ content.php
@@ -19,7 +19,7 @@
     <p>
 
     <?php
-    $article_tags=wp_get_post_tags(get_the_id());
+    $article_tags = wp_get_post_tags(get_the_id());
     foreach ($article_tags as $tag_article) {
     ?>
 
@@ -28,22 +28,51 @@
     <?php } ?>
     </p>
 
-    <p><span class="subscribe-topic"><a href="#">SUBSCRIBE TOPIK INI</a></span><p>
+    <p><span class="subscribe-topic"><a href="#">SUBSCRIBE TOPIK INI</a></span>  <span style="color:#d4d4d4;padding:0px 20px;">BAGI ARTIKEL INI</span><p>
+    <p style="margin-top:50px"><h3 class="article-tittle">Yuk daftarkan email kamu dan jangan sampai ketinggalan<br> artikel-artikel menarik dari UntukIbu.</h3></p>
+    <p>
+      <form>
+        <div class="col-sm-9 no-padding form-group">
+          <div class="input-group">
+            <input class="form-control" placeholder="Email Kamu" type="text">
+            <span class="input-group-btn">
+              <button class="btn btn-success" type="button">DAFTAR</button>
+            </span>
+          </div>
+        </div>
+      </form>
+      <div class="clearfix">
 
+      </div>
+    </p>
+    <h2 style="color:#d4d4d4;margin-top:50px;font-wight:100px;">Artikel Lainya</h2>
   </div>
   <div class="container-fluid no-padding m-b-40">
-    <div class="col-sm-4 " style="background:yellow; padding:220px 0px 0px">
+    <?php
+    $post_singleCat = wp_get_post_categories(get_the_id());
+
+    $args = array('posts_per_page' => 3,'category__and' => $post_singleCat, 'meta_key' => 'wpb_post_views_count', 'order_by' => 'meta_value_num', 'order' => 'DESC');
+    $postbycats = wp_get_recent_posts($args);
+
+    foreach ($postbycats as $postbycat){
+      //$img_postbycat = the_field('square_feature_image',$postbycat["ID"]);
+    ?>
+    <div class="col-sm-4 " style="background:url('<?php the_field('square_feature_image', $postbycat['ID']); ?>'); background-size:cover; padding:220px 0px 0px">
       <div class="inner" style="padding: 20px;
-background: rgba(225,225,225,0.7);">
-        <h4 class="article-tittle" >Panduan Memilih Baby Carrier yang Teapt</h4>
+        background: rgba(225,225,225,0.7);">
+        <h4 class="article-tittle"><?php  echo $postbycat['post_title']; ?></h4>
         <a href="#">Baca Selengkapnya</a>
       </div>
     </div>
-    <div class="col-sm-4 " style="background:blue;">
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-    </div>
-    <div class="col-sm-4 " style="background:red">
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-    </div>
+    <?php
+    }
+    ?>
+  </div>
+  <div class="container content article-content">
+    <?php
+      if ( comments_open() || get_comments_number() ) :
+        comments_template();
+      endif;
+    ?>
   </div>
 </div>
